0911ed48f04243dffcede6775587ac7b469fe675
Alexei <gararaevalexei@yandex.com>
2024-12-23 18:34:01 +0300

    [+] almost working wifi

--- diplom/diplom.ino
+++ diplom/diplom.ino
@@ -28,9 +28,16 @@
 #define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Настройки точки доступа
-const char *ssid = "ESP32_AP";
-const char *password = "12345678";
+// Настройка режима Wi-Fi
+#define WIFI_MODE_AP 0 // Установите 1 для AP, 0 для STA
+
+// Настройки Wi-Fi в режиме клиента (STA)
+const char *sta_ssid = "applied_robotics";
+const char *sta_password = "listentome";
+
+// Настройки точки доступа (AP)
+const char *ap_ssid = "ESP32_AP";
+const char *ap_password = "12345678";
 
 // Веб-сервер
 WebServer server(80);
@@ -54,7 +61,7 @@ void setup() {
   display.println(F("Инициализация SD..."));
   display.display();
 
-    // Настройка подтяжки для всех линий SPI
+  // Настройка подтяжки для всех линий SPI
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH); // Включаем подтяжку на CS (HIGH как default state)
   pinMode(SCK_PIN, INPUT_PULLUP);  // Подтяжка для Clock
@@ -75,39 +82,74 @@ void setup() {
   display.println(F("SD Card OK!"));
   display.display();
 
-  // Запуск точки доступа
-  WiFi.softAP(ssid, password);
-
-  IPAddress IP = WiFi.softAPIP();
-  Serial.println("Точка доступа запущена:");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
-  Serial.print("Password: ");
-  Serial.println(password);
-  Serial.print("IP Address: ");
-  Serial.println(IP);
-
-  // Вывод данных на дисплей
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(F("Wi-Fi Access Point"));
-  display.println("-------------------");
-  display.print(F("SSID: "));
-  display.println(ssid);
-  display.print(F("Password: "));
-  display.println(password);
-  display.print(F("IP: "));
-  display.println(IP.toString());
-  display.display();
+  // Инициализация Wi-Fi
+  if (WIFI_MODE_AP) {
+    setupWiFiAP();
+  } else {
+    setupWiFiSTA();
+  }
 
   // Настройка маршрутов веб-сервера
   server.on("/", handleRoot);
-  server.on("/upload", HTTP_POST, handleFileUpload);
+  server.on("/upload", HTTP_POST, []() {
+    server.send(200, "text/plain; charset=UTF-8", "Файл успешно загружен");
+  }, handleFileUpload);
   server.on("/list", HTTP_GET, handleFileList);
   server.on("/read", HTTP_GET, handleFileRead);
   server.begin();
 
   Serial.println("Веб-сервер запущен");
+}
+
+void setupWiFiAP() {
+  WiFi.softAP(ap_ssid, ap_password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.println("Точка доступа запущена:");
+  Serial.print("SSID: ");
+  Serial.println(ap_ssid);
+  Serial.print("Password: ");
+  Serial.println(ap_password);
+  Serial.print("IP Address: ");
+  Serial.println(IP);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi AP Mode");
+  display.println("-------------------");
+  display.print("SSID: ");
+  display.println(ap_ssid);
+  display.print("Password: ");
+  display.println(ap_password);
+  display.print("IP: ");
+  display.println(IP.toString());
+  display.display();
+}
+
+void setupWiFiSTA() {
+  WiFi.begin(sta_ssid, sta_password);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi Client Mode");
+  display.println("-------------------");
+  display.print("Connecting to: ");
+  display.println(sta_ssid);
+  display.display();
+
+  Serial.print("Подключение к Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nПодключено к Wi-Fi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  display.setCursor(0, 30);
+  display.print("IP: ");
+  display.println(WiFi.localIP().toString());
+  display.display();
 }
 
 void loop() {
@@ -118,8 +160,8 @@ void loop() {
 void handleRoot() {
   String html = R"rawliteral(
     <!DOCTYPE html>
-    <meta charset="UTF-8">
     <html>
+    <meta charset="UTF-8">
     <body>
       <h1>SD Card Web Server</h1>
       <h2>Загрузить файл:</h2>
@@ -137,48 +179,44 @@ void handleRoot() {
     </body>
     </html>
   )rawliteral";
-  server.send(200, "text/html; charset=UTF-8", html); // Указана кодировка UTF-8
+  server.send(200, "text/html; charset=UTF-8", html);
 }
 
 File uploadFile; // Глобальная переменная для текущего файла
+
+void logRequest() {
+  Serial.printf("Метод: %s\n", server.method() == HTTP_GET ? "GET" : "POST");
+  Serial.printf("URI: %s\n", server.uri().c_str());
+  for (uint8_t i = 0; i < server.args(); i++) {
+    Serial.printf("Аргумент %s: %s\n", server.argName(i).c_str(), server.arg(i).c_str());
+  }
+}
 
 void handleFileUpload() {
   HTTPUpload &upload = server.upload();
 
   if (upload.status == UPLOAD_FILE_START) {
     String filename = "/" + upload.filename;
+    filename.replace(" ", "_");
     Serial.printf("Начало загрузки файла: %s\n", filename.c_str());
-    
-    // Открываем файл на запись
-    uploadFile = SD.open(filename, FILE_WRITE);
-    if (!uploadFile) {
+
+    File file = SD.open(filename, FILE_WRITE);
+    if (!file) {
       Serial.println("Ошибка открытия файла для записи");
-      server.send(500, "text/plain; charset=UTF-8", "Ошибка открытия файла для записи");
       return;
     }
-  } 
-  else if (upload.status == UPLOAD_FILE_WRITE) {
-    // Записываем текущий блок данных
-    if (uploadFile) {
-      uploadFile.write(upload.buf, upload.currentSize);
-    } else {
-      Serial.println("Файл не открыт на запись");
-      server.send(500, "text/plain; charset=UTF-8", "Ошибка записи в файл");
-      return;
-    }
-  } 
-  else if (upload.status == UPLOAD_FILE_END) {
-    // Закрываем файл после завершения загрузки
-    if (uploadFile) {
-      uploadFile.close();
-      Serial.println("Файл успешно записан на SD-карту");
-      server.send(200, "text/plain; charset=UTF-8", "Файл успешно загружен");
-    } else {
-      Serial.println("Ошибка завершения записи");
-      server.send(500, "text/plain; charset=UTF-8", "Ошибка завершения записи");
-    }
+
+    file.close();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    Serial.printf("Записано %d байт\n", upload.currentSize);
+  } else if (upload.status == UPLOAD_FILE_END) {
+    Serial.println("Загрузка завершена");
+  } else {
+    Serial.println("Неизвестный статус");
   }
 }
+
+
 
 
 void handleFileList() {
